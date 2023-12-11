c97bee5b65a08030a0549519b3644abf30ec3a06
Shrey K <srkkadia@gmail.com>
2023-12-11 19:57:24 +0530

    tasks for Throw keyword

--- Exception Handling/Throww.java
+++ Exception Handling/Throww.java
@@ -27,3 +27,16 @@ public class Throww {
         }
     }
 }
+
+/*
+Tasks:
+
+1. Custom Exception in Banking System: Develop a simplified banking system in Java that utilizes custom exceptions using the 'throw' keyword. Implement scenarios where 'throw' is used to handle insufficient balance or invalid transaction exceptions.
+
+2. Input Validation in a User Registration System: Write a Java program for a user registration system where the 'throw' keyword is employed to manage input validation errors. For instance, use 'throw' to handle cases like invalid email formats or missing required fields during user registration.
+
+3. Exception Handling in a Calculator: Design a calculator program in Java that employs the 'throw' statement to manage arithmetic exceptions. Create situations where 'throw' is used to handle divide-by-zero errors or invalid mathematical operations.
+
+4. Network Connection Error Handling: Develop a network handling module in Java where the 'throw' keyword is used to manage connection errors. Simulate scenarios where 'throw' is employed to handle cases like timeouts or unreachable hosts during network operations.
+
+*/
